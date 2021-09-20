8a9277a60069fe1d8533402524e6a08f608e71a1
DKSander21 <91073099+DKSander21@users.noreply.github.com>
2021-09-20 16:25:13 +0100

    Update ukpostcode.js

--- ukpostcode.js
+++ ukpostcode.js
@@ -1,4 +1,3 @@
-<script>
   window.ub.form.customValidators.UKPostalCode = {
     isValid: function(value) {
       return /^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$/.test(value);
@@ -6,4 +5,3 @@
 
     message: 'Please enter a valid UK postcode',
   };
-</script>
